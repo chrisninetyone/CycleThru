0eeff9a55b492e5cacfe0bbe10f2ce8d271c9d0c
Joe Walker <jpdwalker@gmail.com>
2019-06-03 18:35:05 +0800

    added show to user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # get 'profile', to: 'pages#profile', as: :profile
   # get 'about', to: 'pages#about', as :about
-
+  resources :user, only: [:show]
   resources :trips, only: [:show, :new, :create, :update]
   resources :points, only: [:index, :show, :new, :create] do
     resources :posts, only: [:new, :create, :update, :edit, :delete]
