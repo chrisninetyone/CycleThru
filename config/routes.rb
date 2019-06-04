c536f27a066bb3e75eeae2976e2cb47519841b1f
Christopher Foley <chrisninetyone@gmail.com>
2019-06-04 15:02:55 +0800

    configure omniauth with le wagon tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root to: 'pages#home'
 
   # get 'profile', to: 'pages#profile', as: :profile
