e43a0729ae6692dd30e13d274f6901ff5ed9a7f2
Christopher Foley <chrisninetyone@gmail.com>
2019-06-05 12:02:33 +0800

    installed mapbox-directions package and implemented on map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -13,36 +13,43 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
-  });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
-    element.style.backgroundSize = 'contain';
-    element.style.width = '25px';
-    element.style.height = '25px';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
 
-    new mapboxgl.Marker(element)
-    .setLngLat([ marker.lng, marker.lat ])
-    .addTo(map);
-  });
-  fitMapToMarkers(map, markers);
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    });
+    fitMapToMarkers(map, markers);
+
+    map.addControl(new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/cycling'
+    }), 'top-left');
   }
 };
 
 
-const getDirections = (startPoint, endPoint) => {
-  //document.querySelector -- get input values from
-  //make an api call here
-  //get returned
+// const getDirections = (startPoint, endPoint) => {
+//   //document.querySelector -- get input values from
+//   //make an api call here
+//   //get returned
 
 
-  //Be able to click save on a route to make a POST request to our DB
-}
+//   //Be able to click save on a route to make a POST request to our DB
+// }
 
 //1. making api call
 
