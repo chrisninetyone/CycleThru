8eeba29eeec18ad1f05456c9b2a3c2c135871652
Joe Walker <jpdwalker@gmail.com>
2019-06-06 18:46:00 +0800

    skeleton made of user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
+    @total_distance = calculate_distance
     authorize @user
+  end
+
+  private
+
+  def calculate_distance
+    # total km of routes
   end
 end
