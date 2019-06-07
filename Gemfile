3c0d3d8b3ae5f70c013fe05e8ddef9f660b16c0f
Christopher Foley <chrisninetyone@gmail.com>
2019-06-07 16:29:13 +0800

    another step toward passing params to create a trip

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'pundit'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
 gem 'omniauth-facebook'
+gem 'simple_token_authentication'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :trips
-  has_many :points
+  acts_as_token_authenticatable
 
+  has_many :trips
+  has_many :points_id
   validates :first_name, presence: true
   validates :last_name, presence: true
   # validates :email, presence: true
