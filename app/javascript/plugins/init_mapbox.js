6cc8168ec8c490f0284161f5f7460f9235e77fde
sarah wolfson <swolfson23@gmail.com>
2019-06-11 16:35:05 +0800

    changes to the points JS file

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,13 +1,10 @@
 import mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
-
-
 const createMarkersForMap = (mapElement, map) => {
   const markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach((marker) => {
@@ -15,13 +12,10 @@ const createMarkersForMap = (mapElement, map) => {
     element.className = 'marker';
     element.style.backgroundImage = `url('${marker.image_url}')`;
     element.style.backgroundSize = 'contain';
-
     element.style.width = '25px';
     element.style.height = '25px';
     element.style.cursor = "pointer"
-
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
     new mapboxgl.Marker(element)
     .setLngLat([ marker.lng, marker.lat ])
     .addTo(map)
@@ -29,56 +23,42 @@ const createMarkersForMap = (mapElement, map) => {
   });
   // fitMapToMarkers(map, markers);
 }
-
-
-
-const initMapbox = () => {
+const initMapbox = (currentLocation) => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-
     console.log(mapElement.dataset.currentLocation);
-
     const mapParams = {
       container: 'map',
       style: 'mapbox://styles/swolfson/cjwpspnit8u8v1cpbotrhatm3',
-      center: JSON.parse(mapElement.dataset.currentLocation),
       zoom: 15
     }
-
-    // mapParams.center = JSON.parse(mapElement.dataset.currentLocation)
-    // console.log("newest", mapElement.dataset.currentLocation)
+    mapParams.center = currentLocation
+    // console.log("later", mapParams.center)
     const map = new mapboxgl.Map(mapParams);
-
     //add current position marker
-
     const geolocate = new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true
       },
       trackUserLocation: true
     });
-
     map.addControl(geolocate);
-
     geolocate.on('geolocate', (e) => {
       const long = e.coords.longitude;
       const lat = e.coords.latitude
       const position = [long, lat];
       console.log('test', position);
     });
-
     createMarkersForMap(mapElement, map);
-
+    const userLocation = new mapboxgl.Marker();
     let condition = true
-
     map.on('dblclick', (e) => {
       console.log(e)
       if (condition){
       const popup = new mapboxgl.Popup().setHTML(
         `<button type="button" data-toggle="modal" data-target="#pointModal">Create Point</button>
         `);
-
       const draggable = new mapboxgl.Marker({
         draggable: true
       })
@@ -89,45 +69,38 @@ const initMapbox = () => {
       const long = document.querySelector("#point_long");
       lat.value = e.lngLat.lat;
       long.value = e.lngLat.lng;
-
-      document.querySelector("#coordinates").hidden = true;
-
       const onDragEnd = () => {
+
         const lngLat = draggable.getLngLat();
-        document.querySelector("#coordinates").value = lngLat;
+      const lat = document.querySelector("#point_lat");
+      const long = document.querySelector("#point_long");
+      lat.value = lngLat.lat;
+      long.value = lngLat.lng;
     }
-
    draggable.on('dragend', onDragEnd);
-
     condition = false
       }
     });
-
-
-    map.addControl(new MapboxDirections({
+    const mapboxDirections = new MapboxDirections({
       accessToken: mapboxgl.accessToken,
       unit: 'metric',
-      profile: 'mapbox/cycling'
-    }), 'top-left');
-
+      profile: 'mapbox/cycling',
+      interactive: false
+    })
+    map.addControl(mapboxDirections, 'top-left');
+    //   console.log('alakazam')
+    //   mapboxDirections.interactive(true)
+    // }, 5000)
     //hide the options for driving and walking
     const option = document.querySelector('.mapbox-directions-profile');
     option.hidden = true;
-
     //define the directions and directionsHeader to be able to toggle them
     const directions = document.querySelector('.directions-control-instructions');
     const directionsHeader = document.querySelector(".mapbox-directions-route-summary");
-
     //insert button after the to and from form on map
     document.querySelector('.directions-control-inputs').insertAdjacentHTML('afterend', `<button id="toggler" class="btn btn-sm btn-dark m-2">Toggle Directions</button>`);
-
-
-
-
-
     //Hide directions and add an event listener on the button to toggle "hidden" class in _map.scss
     // directions.hidden = true;
-
     document.querySelector('#toggler').addEventListener('click', () => {
       if (directions.hidden === false) {
         directions.hidden = true
@@ -135,10 +108,6 @@ const initMapbox = () => {
         directions.hidden = false
       }
     })
-
   }
-
 };
-
-
 export { initMapbox };
